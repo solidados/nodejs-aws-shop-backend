7a5ae8268697eac8a33f1b8c4b3fa679cfed4ae0
solidados <geekbrains.pk@internet.ru>
2024-06-23 21:22:13 +0400

    refactor: update errorHandlers

--- product-service/lambda-functions/errorHandler.ts
+++ product-service/lambda-functions/errorHandler.ts
@@ -1,20 +1,16 @@
 import { APIGatewayProxyResult } from "aws-lambda";
-
-interface ErrorResponse {
-  statusCode: number;
-  message: string;
-}
+import { ErrorResponse } from "./error.interface";
 
 const errorMap: Record<string, ErrorResponse> = {
-  NotFoundError: { statusCode: 404, message: "Not found" },
-  BadRequestError: { statusCode: 400, message: "Bad request" },
-  InternalServerError: { statusCode: 500, message: "Internal server error" },
+  NotFoundError: { statusCode: 404, message: "404 Not found" },
+  BadRequestError: { statusCode: 400, message: "400 Bad request" },
+  InternalServerError: { statusCode: 500, message: "500 Internal server error" },
 };
 
 export function handleAPIGatewayError(error: any): APIGatewayProxyResult {
   const defaultError: ErrorResponse = {
     statusCode: 500,
-    message: "Unknown error occurred",
+    message: `Error occurred: ${error}`,
   };
 
   if (error instanceof Error && error.name in errorMap) {
@@ -42,15 +38,15 @@ function createErrorResponse(
 }
 
 export class NotFoundError extends Error {
-  constructor(message: string) {
-    super(message);
+  constructor() {
+    super("Not found error");
     this.name = "NotFoundError";
   }
 }
 
 export class BadRequestError extends Error {
-  constructor(message: string) {
-    super(message);
+  constructor() {
+    super("Bad request error");
     this.name = "BadRequestError";
   }
 }
