a0c81fd3e70b5dd6c4245b10154a28c44cb4cd09
solidados <geekbrains.pk@internet.ru>
2024-06-28 17:48:24 +0400

    chore: update ImportServiceStack with importProductsFile function

--- import-service/lib/import-service-stack.ts
+++ import-service/lib/import-service-stack.ts
@@ -1,16 +1,36 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { RemovalPolicy } from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as s3 from "aws-cdk-lib/aws-s3";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as apigateway from "aws-cdk-lib/aws-apigateway";
 
 export class ImportServiceStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const importServiceS3Bucket: cdk.aws_s3.IBucket = s3.Bucket.fromBucketName(
+      this,
+      "ImportServiceS3Bucket",
+      "import-service-s3-vedro",
+    );
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'ImportServiceQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const importProductsFileFunction: cdk.aws_lambda.Function =
+      new lambda.Function(this, "importProductsFileFunction", {
+        runtime: lambda.Runtime.NODEJS_20_X,
+        code: lambda.Code.fromAsset("lambda-functions"),
+        handler: "importProductsFile.handler",
+        environment: {
+          BUCKET_NAME: importServiceS3Bucket.bucketName,
+        },
+      });
+
+    importServiceS3Bucket.grantReadWrite(importProductsFileFunction);
+
+    const api = new apigateway.RestApi(this, "import-service-api", {
+      restApiName: "Import Service API",
+      cloudWatchRole: true,
+      cloudWatchRoleRemovalPolicy: RemovalPolicy.DESTROY,
+    });
   }
 }
