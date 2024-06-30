8e93e7d232bfedbf603f316f30057a72e490c56d
solidados <geekbrains.pk@internet.ru>
2024-06-30 14:26:08 +0400

    refactor: create importProductsFile.class to simplify the import-service-stack

--- import-service/lib/utils/importProductsFile.class.ts
+++ import-service/lib/utils/importProductsFile.class.ts
@@ -1,31 +1,23 @@
 import * as lambda from "aws-cdk-lib/aws-lambda";
-import * as s3 from "aws-cdk-lib/aws-s3";
 import { Construct } from "constructs";
 import { ImportS3Bucket } from "./importS3Bucket.class";
-import * as s3_notifications from "aws-cdk-lib/aws-s3-notifications";
 
-export class ImportFileParser {
+export class ImportProductsFile {
   public readonly lambdaFunction: lambda.Function;
 
   constructor(scope: Construct, id: string, bucket: ImportS3Bucket) {
     this.lambdaFunction = new lambda.Function(
       scope,
-      "importFileParserFunction",
+      "importProductsFileFunction",
       {
         runtime: lambda.Runtime.NODEJS_20_X,
         code: lambda.Code.fromAsset("lambda-functions"),
-        handler: "importFileParser.handler",
+        handler: "importProductsFile.handler",
         environment: { BUCKET_NAME: bucket.bucket.bucketName },
       },
     );
+
     bucket.bucket.grantReadWrite(this.lambdaFunction);
     bucket.bucket.grantPut(this.lambdaFunction);
-    bucket.bucket.grantDelete(this.lambdaFunction);
-
-    bucket.bucket.addEventNotification(
-      s3.EventType.OBJECT_CREATED,
-      new s3_notifications.LambdaDestination(this.lambdaFunction),
-      { prefix: "uploaded/" },
-    );
   }
 }
