945c96c37867b0ed059dc67448697e093e82c2f7
solidados <geekbrains.pk@internet.ru>
2024-07-07 19:15:01 +0400

    refactor: update importFileParser.class for SQS send messages to LambdaFunction

--- import-service/lib/utils/importFileParser.class.ts
+++ import-service/lib/utils/importFileParser.class.ts
@@ -3,11 +3,20 @@ import * as s3 from "aws-cdk-lib/aws-s3";
 import { Construct } from "constructs";
 import { ImportS3Bucket } from "./importS3Bucket.class";
 import * as s3_notifications from "aws-cdk-lib/aws-s3-notifications";
+import { IQueue } from "aws-cdk-lib/aws-sqs";
 
+interface ImportFileParserProps {
+  bucket: ImportS3Bucket;
+  catalogItemsQueue: IQueue;
+}
 export class ImportFileParser {
   public readonly lambdaFunction: lambda.Function;
 
-  constructor(scope: Construct, id: string, bucket: ImportS3Bucket) {
+  constructor(
+    scope: Construct,
+    id: string,
+    { bucket, catalogItemsQueue }: ImportFileParserProps,
+  ) {
     this.lambdaFunction = new lambda.Function(
       scope,
       "importFileParserFunction",
@@ -15,7 +24,10 @@ export class ImportFileParser {
         runtime: lambda.Runtime.NODEJS_20_X,
         code: lambda.Code.fromAsset("lambda-functions"),
         handler: "importFileParser.handler",
-        environment: { BUCKET_NAME: bucket.bucket.bucketName },
+        environment: {
+          BUCKET_NAME: bucket.bucket.bucketName,
+          SQS_URL: catalogItemsQueue.queueUrl,
+        },
       },
     );
     bucket.bucket.grantReadWrite(this.lambdaFunction);
@@ -27,5 +39,7 @@ export class ImportFileParser {
       new s3_notifications.LambdaDestination(this.lambdaFunction),
       { prefix: "uploaded/" },
     );
+
+    catalogItemsQueue.grantSendMessages(this.lambdaFunction);
   }
 }
